1f713fd9f57bb629f6d03e8841a797fc76236cf5
LyteFM <lytefm@live.de>
2017-08-05 14:25:11 +0200

    yo

--- Main.java
+++ Main.java
@@ -4,4 +4,8 @@ package dicograph;
  * Created by Fynn Leitow on 05.08.17.
  */
 public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
 }
