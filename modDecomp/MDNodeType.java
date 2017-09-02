b71f752bc633c8e60609b046dbb824cb5cb144b2
LyteFM <lytefm@live.de>
2017-09-02 15:04:33 +0200

    Adding mod decomp from Tedder

---
+++ modDecomp/MDNodeType.java
@@ -0,0 +1,13 @@
+package dicograph.modDecomp;
+
+/*
+ * The different types of nodes in a modular decomposition tree. 
+ */
+enum MDNodeType {
+	PRIME,SERIES,PARALLEL;
+
+	/* Returns true iff this type is degenerate. */
+	public boolean isDegenerate() {
+		return (this == PARALLEL || this == SERIES);
+	}
+}
