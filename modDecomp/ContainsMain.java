242100e411210e8326774c74bb9000d5b9dda43c
LyteFM <lytefm@live.de>
2017-10-10 14:31:21 +0200

    Make MDTree readible

--- modDecomp/ContainsMain.java
+++ modDecomp/ContainsMain.java
@@ -14,9 +14,48 @@ public class ContainsMain {
 
         // old läuft korrekt.
 		GraphHandle g = new GraphHandle(filePath);
-		System.out.println("Old Code:\n" + g.getMDTreeOld());
+		String g_res = g.getMDTreeOld().toString();
+		System.out.println("Old Code:\n" + beautify(g_res));
 
 		GraphHandle g2 = new GraphHandle(filePath);
-		System.out.println("\nNew Code:\n" + g2.getMDTree());
+		String g2_res = g2.getMDTree().toString();
+		System.out.println("\nNew Code:\n" + beautify(g2_res));
 	}
+
+	static String beautify(String mdTree){
+	    StringBuilder ret = new StringBuilder();
+	    int offsetMultiplier = 0;
+	    char previous = '(';
+	    for(char c : mdTree.toCharArray()){
+	        if(c == '('){
+                ret.append("\n");
+	            addOffset(ret,offsetMultiplier);
+                ret.append(c);
+                offsetMultiplier++;
+            } else if (c == ')'){
+                offsetMultiplier--;
+                // new line only for modules, not for vertices.
+	            if(previous == ')') {
+                    ret.append("\n");
+                    addOffset(ret,offsetMultiplier);
+                }
+                ret.append(c);
+            } else {
+                ret.append(c);
+            }
+            previous = c;
+        }
+
+
+	    return ret.toString();
+
+    }
+
+    static StringBuilder addOffset(StringBuilder builder, int times){
+        String offset = "  ";
+        for(int i = 0; i< times; i++){
+	        builder.append(offset);
+        }
+        return builder;
+    }
 }
