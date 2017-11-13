f4ccdd8078292870dd6c61ddff13d61285447afb
LyteFM <lytefm@live.de>
2017-11-13 14:40:19 +0100

    Small OO refactoring

--- modDecomp/DirectedMD.java
+++ modDecomp/DirectedMD.java
@@ -21,14 +21,9 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.ListIterator;
 import java.util.Map;
-import java.util.Set;
 import java.util.TreeSet;
-import java.util.function.Function;
 import java.util.logging.Logger;
-
-import dicograph.utils.SortAndCompare;
 
 
 /**
@@ -218,9 +213,10 @@ public class DirectedMD {
 
         // Step 4: At each O-complete and 1-complete node X of T(H), order the children s.t.
         //         each equivalence class of R_X is consecutive.
+        // AND
         // Step 5: At each  2-complete node Y, select an arbitrary set S of representatives from the children.
         //         order the children of Y according to a perfect factorizing permutation of G[S].
-        computeFactorizingPermutation(TreeForH);
+        TreeForH.computeFactorizingPermutationAndReorderAccordingly(log, inputGraph, nVertices );
 
 
 //        SimpleDirectedGraph<Integer, DefaultEdge> inducedSubgraphOf2completeNode = new SimpleDirectedGraph<>(DefaultEdge.class);
@@ -231,6 +227,7 @@ public class DirectedMD {
         // Step 6: Resulting leaf order of T(H) is a factorizing permutation of G by Lem 20,21. Use algorithm
         //         [2] to find the modular decomposition of G.
         // todo: wat? the left-to-right-order?
+        // todo: I already have more than just the permutation (as Tedder says!). Use that Tree!
         ArrayList<PartitiveFamilyLeafNode> trueLeafOrder =  new ArrayList<>(nVertices);
         TreeForH.getLeavesInLeftToRightOrder( trueLeafOrder );
 
@@ -243,246 +240,6 @@ public class DirectedMD {
 
     }
 
-    /*
-    protected static List<Integer> perfectFactPermOfTournament(SimpleDirectedGraph<Integer, DefaultEdge> tournament, Logger log){
-
-        int n = tournament.vertexSet().size();
-        ArrayList<Integer> ret = new ArrayList<>(n);
-        HashMap<Integer, Collection<Integer>> vertexToPartition = new HashMap<>(n*4/3);
-        ArrayList<Collection<Integer>> partitions = new ArrayList<>(n);
-        // init: P_0 = V. This also defines the vertex Indices.
-        List<Integer> VList = new ArrayList<>(tournament.vertexSet());
-        partitions.add(VList);
-        for(int vertex : VList) {
-            vertexToPartition.put(vertex, VList);
-        }
-
-        for(int i = 0; i<n; i++){
-
-            int realVertexNo = VList.get(i); // unnecessary, if int-vertices from 0 to n-1. necessary, if arbitrary.
-            Collection<Integer> cPartition = vertexToPartition.get(realVertexNo);
-            int partitionsIndex = partitions.indexOf(cPartition);
-
-            // skip singletons
-            if(cPartition.size() > 1) {
-                // neighborhood N_- and N_+:
-                Set <DefaultEdge> incoming = tournament.incomingEdgesOf(realVertexNo);
-                HashSet<Integer> inNeighbors = new HashSet<>(incoming.size()*4/3);
-                for( DefaultEdge edge : incoming){
-                    int source = tournament.getEdgeSource(edge);
-                    inNeighbors.add( source );
-                }
-                inNeighbors.retainAll(cPartition); // Compute the intersection
-
-                Set <DefaultEdge> outgoing = tournament.outgoingEdgesOf(realVertexNo);
-                HashSet<Integer> outNeigbors = new HashSet<>(outgoing.size()*4/3);
-                for( DefaultEdge edge : outgoing){
-                    int source = tournament.getEdgeTarget(edge);
-                    outNeigbors.add( source );
-                }
-                outNeigbors.retainAll(cPartition);
-
-                // update partitions and update map
-                // remove the former C
-                vertexToPartition.remove(realVertexNo);
-                partitions.remove(partitionsIndex);
-
-                // add C ∩ N_{-}(v_i)
-                partitions.add(partitionsIndex, inNeighbors);
-                for( int vNo : inNeighbors){
-                    vertexToPartition.put(vNo, inNeighbors);
-                }
-
-                // add singleton {v_i}
-                ArrayList<Integer> singleton = new ArrayList<>(1);
-                singleton.add(realVertexNo);
-                partitions.add(partitionsIndex+1, singleton);
-                vertexToPartition.put(realVertexNo, singleton);
-
-                // add C ∩ N_{+}(v_i)
-                partitions.add(partitionsIndex+2, outNeigbors);
-                for(int vNo : outNeigbors){
-                    vertexToPartition.put(vNo, outNeigbors);
-                }
-
-                // done.
-                if (partitions.size() == n) {
-                    break;
-                }
-            }
-        }
-
-        for(Collection<Integer> singleton : partitions){
-            if(singleton.size() > 1){
-                log.severe(() -> "Error: invalid element " + singleton.toString());
-                log.severe(() -> "In list of partitions " + partitions.toString());
-            }
-            ret.add(singleton.stream().findFirst().get());
-        }
-
-        return ret;
-    }
-    */
-
-    protected void computeFactorizingPermutation (PartitiveFamilyTree treeForH){
-
-        // First step: order the leaves in accordance of their left-right appearance in the Tree
-        List<PartitiveFamilyLeafNode> orderedLeaves = new ArrayList<>(nVertices);
-        treeForH.getLeavesInLeftToRightOrder(orderedLeaves);
-        ArrayList<Integer> permutationAsIntegers = new ArrayList<>(nVertices);
-        for(PartitiveFamilyLeafNode leaf : orderedLeaves){
-            permutationAsIntegers.add(leaf.getVertex());
-        }
-        // the position of every element in the permutation
-        int[] positionInPermutation = new int[nVertices];
-        for(int i = 0; i< permutationAsIntegers.size(); i++){
-            positionInPermutation[permutationAsIntegers.get(i)] = i;
-        }
-
-        // now, iterate through the tree and compute for every inner node X of T_H:
-        //   - le(X), re(X): the first occurence of any vertex of X in σ.
-        //     this can be done bottom-up
-        treeForH.computeReAndLeBottomUp(orderedLeaves);
-
-        //   - lc(X), rc(X): the leftmost/rightmost of its cutters
-        // Therefore: "BucketSort" edges of G according to σ. BitSets guarantee easy symdiff operation.:
-
-        // This is for N_{+}: 1st key is outVertex, 2nd key is destVertex
-        BitSet[] sortedOutEgdes = SortAndCompare.edgesSortedByPerm(permutationAsIntegers, positionInPermutation, inputGraph, true);
-        // This is for N_{-}: 1st key is destVertex, 2nd outVertex
-        BitSet[] sortedInEdges = SortAndCompare.edgesSortedByPerm(permutationAsIntegers, positionInPermutation,  inputGraph, false);
-
-        // now, compute the cutters:
-        treeForH.computeLeftRightCutters(sortedOutEgdes,sortedInEdges);
-
-        // remember: X is a module iff le(X) == lc(X) and re(X) == rc(X)
-        // next step: use N_{+} and N_{-} to separate the children of a 0/1-complete node that is a module into R_X-classes
-        // todo: according to step 5, I can select any representative of its children. Is my subgraph okay???
-        treeForH.reorderAllNodes(log, sortedOutEgdes, sortedInEdges, orderedLeaves, positionInPermutation);
-
-
-    }
-
-
-
-    /**
-     * Implementation of the Inclusion tree according to Lemma 11
-     *
-     * @param inputSet a Collection of BitSets representing a partitive set family
-     * @return The rooted tree of the input set
-     */
-    protected PartitiveFamilyTree getInclusionTreeFromBitsets(Collection<BitSet> inputSet) {
-
-        HashMap<BitSet, RootedTreeNode> bitsetToInclusionTreenNode = new HashMap<>(inputSet.size()*4/3);
-
-        // Step 0: eliminate doubles from input and add root, if not yet present.
-        HashSet<BitSet> inputSetNoDoubles = new HashSet<>(inputSet);
-        if (debugMode && inputSetNoDoubles.size() != inputSet.size()) {
-            log.warning("Double entry in input for inclusion tree - possible merge in overlap components.");
-        }
-        ArrayList<BitSet> inputNodeList = new ArrayList<>(inputSetNoDoubles);
-
-        // Sort the array by size, using counting sort
-        // todo: Das brauche ich häufiger! Auch hier mit BucketSort...
-        inputNodeList.sort( // descending size, with lamdas.
-                (b1, b2) -> Integer.compare(b2.cardinality(), b1.cardinality()));
-        log.fine("Input sorted by size: " + inputNodeList);
-
-        // add root, if not yet included
-        BitSet rootSet = new BitSet(nVertices);
-        rootSet.set(0, nVertices); // toIndex must be n
-        if(!inputSetNoDoubles.contains(rootSet)) {
-            inputNodeList.add(0, rootSet);
-        }
-
-
-        // Create a List for each v ∈ V of the members of F (i.e. the elements of σ) containing v in ascending order of their size:
-
-        // init empty
-        ArrayList<LinkedList<BitSet>> xLists = new ArrayList<>(nVertices);
-        for (int i = 0; i < nVertices; i++) {
-            xLists.add(i, new LinkedList<>());
-        }
-
-
-
-        // - visit each Y ∈ F in descending order of size. For each x ∈ Y, insert pointer to Y to front of x's list. [O(sz(F)]
-        inputNodeList.forEach(ySet ->
-                ySet.stream().forEach(i ->
-                        xLists.get(i).addFirst(ySet)
-                )
-        );
-        // root is now the last element in every xList
-
-
-        PartitiveFamilyTree ret = new PartitiveFamilyTree();
-        PartitiveFamilyTreeNode root = new PartitiveFamilyTreeNode(rootSet);
-        ret.setRoot(root);
-        //HashMap<BitSet, RootedTreeNode> bitsetToOverlapTreenNode = new HashMap<>(nontrivOverlapComponents.size() * 4 / 3);
-        bitsetToInclusionTreenNode.put(rootSet, root);
-
-        // This creates the inclusion tree from the x's lists
-        // - visit each x ∈ V, put a parent pointer from each member of x's list to its successor in x's list (if not already done)
-        //      -> these are chains of ancestors of {x}
-
-        PartitiveFamilyLeafNode[] allLeafs = new PartitiveFamilyLeafNode[nVertices]; // todo: was damit? Muss ja mal bottom-up?
-        int relationCount = 0;
-        for (int vertexNr = 0; vertexNr < nVertices; vertexNr++) {
-
-            // retrieve List and create leafNode
-            LinkedList<BitSet> currVertexList = xLists.get(vertexNr);
-            PartitiveFamilyLeafNode leafNode = new PartitiveFamilyLeafNode(vertexNr);
-
-            boolean firstEntry = true;
-            ListIterator<BitSet> vListIter = currVertexList.listIterator(0);
-            while (vListIter.hasNext()) {
-
-                BitSet currModule = vListIter.next();
-                // create Trenode if not yet present
-                PartitiveFamilyTreeNode currTreenode = (PartitiveFamilyTreeNode) bitsetToInclusionTreenNode.getOrDefault(currModule, null);
-                if (currTreenode == null) {
-                    currTreenode =  new PartitiveFamilyTreeNode(currModule);
-                    bitsetToInclusionTreenNode.put(currModule, currTreenode);
-                }
-
-                // the first entry of the list always gets the leaf attached.
-                if (firstEntry) {
-                    currTreenode.addChild(leafNode);
-                    allLeafs[vertexNr] = leafNode;
-                    firstEntry = false;
-                }
-
-                // add as child to the next element of xList
-                // Note: currTreenode.equals(root) occurs when we're at the end of the list
-                // Don't check for parent of root, of course.
-                if (vListIter.hasNext()) {
-                    BitSet parentModule = vListIter.next();
-                    PartitiveFamilyTreeNode parentTreeNode = (PartitiveFamilyTreeNode) bitsetToInclusionTreenNode.get(parentModule);
-
-                    // create parent if not yet present
-                    if (parentTreeNode == null) {
-                        parentTreeNode = new PartitiveFamilyTreeNode(parentModule);
-                        bitsetToInclusionTreenNode.put(parentModule, parentTreeNode);
-                        log.fine("Vertex " + vertexNr + ": Created Parent " + parentModule);
-                    }
-
-                    // add parent if current treenode has no parent
-                    if (currTreenode.isRoot()) {
-                        parentTreeNode.addChild(currTreenode);
-                        relationCount++;
-                        log.fine("Vertex " + vertexNr + ": Rel " + relationCount + ": child " + currModule + " to parent " + parentModule);
-                    }
-                    // go one element back in list
-                    vListIter.previous();
-                }
-
-            }
-
-        }
-        ret.setModuleToTreenode(bitsetToInclusionTreenNode);
-
-        return ret;
-    }
 
     // todo: possible to use the same edge Objects???
     int getEdgeValueForH(int u, int v) {
@@ -614,7 +371,7 @@ public class DirectedMD {
 
         // 3.) @Lemma 11: compute the inclusion tree of σ(T_a, T_b)
         // The previously ommitted V and the singleton sets are also added to the tree.
-        RootedTree overlapInclusionTree = getInclusionTreeFromBitsets(overlapComponents.values());
+        RootedTree overlapInclusionTree = new PartitiveFamilyTree(overlapComponents.values(), log, nVertices);
         log.info("Inclusion tree of overlap components: " + MDTree.beautify(overlapInclusionTree.toString()));
         HashMap<BitSet, RootedTreeNode> bitsetToOverlapTreenNode = overlapInclusionTree.getModuleToTreenode();
 
@@ -702,7 +459,7 @@ public class DirectedMD {
         // 7. ) From that set Family, The Inclusion Tree can be constructed by Lem 11.
         // (Cor 19 requires a permutation of the leaves, which will yield the fact perm. So I need the Tree)
 
-        PartitiveFamilyTree ret = getInclusionTreeFromBitsets(strongModulesOfH);
+        PartitiveFamilyTree ret = new PartitiveFamilyTree(strongModulesOfH, log, nVertices);
         // now we have the Tree T(H) = T_a Λ T_b
         log.info("Inclusion Tree: " + ret.toString());
 
--- modDecomp/PartitiveFamilyTree.java
+++ modDecomp/PartitiveFamilyTree.java
@@ -1,19 +1,38 @@
 package dicograph.modDecomp;
 
+import org.jgrapht.DirectedGraph;
+import org.jgrapht.graph.DefaultEdge;
+
+import java.util.ArrayList;
 import java.util.BitSet;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.logging.Logger;
+
+import dicograph.utils.SortAndCompare;
 
 /**
  * Created by Fynn Leitow on 09.11.17.
  */
 public class PartitiveFamilyTree extends RootedTree {
 
-    protected PartitiveFamilyTree(){
+    private PartitiveFamilyTree(){
         super();
+    }
+
+    /**
+     * Creates a new Tree reprenstation from the given vertices-Bitset (Lemma 13)
+     * @param inputSet the BitSet of input vertices representing a partitive family
+     * @param nVertices the number of vertices
+     */
+    public PartitiveFamilyTree(Collection<BitSet> inputSet, Logger log, int nVertices){
+        this();
+        createInclusionTreeFromBitsets(inputSet, log, nVertices);
     }
 
     public void getLeavesInLeftToRightOrder(List<PartitiveFamilyLeafNode> orderedLeaves){
@@ -21,36 +40,182 @@ public class PartitiveFamilyTree extends RootedTree {
         realRoot.getLeavesInLeftToRightOrder(orderedLeaves);
     }
 
-    public void computeReAndLeBottomUp(List<PartitiveFamilyLeafNode> leftOrderedLeaves){
-
-        // init
-        for(int i = 0; i<leftOrderedLeaves.size(); i++){
-            PartitiveFamilyLeafNode leaf = leftOrderedLeaves.get(i);
-            leaf.le_X = i;
-            leaf.re_X = i;
-            // can also be done here
-            leaf.lc_X = i;
-            leaf.rc_X = i;
-        }
-
-        PartitiveFamilyTreeNode rootNode = (PartitiveFamilyTreeNode) root;
-        rootNode.computeReAndLe();
-    }
-
-    public void computeLeftRightCutters(BitSet[] outgoing, BitSet[] incoming){
-        PartitiveFamilyTreeNode rootNode = (PartitiveFamilyTreeNode) root;
-        rootNode.computeLeftRightCutterForThis(outgoing,incoming);
-    }
 
     public void computeAllNodeTypes(DirectedMD data){
         PartitiveFamilyTreeNode rootNode = (PartitiveFamilyTreeNode) root;
         rootNode.determineNodeType(data);
     }
 
-    public void reorderAllNodes(Logger log, BitSet[] outNeighbors, BitSet[] inNeighbors,
-                                List<PartitiveFamilyLeafNode> orderedLeaves, int[] positionInPermutation){
+    public void computeFactorizingPermutationAndReorderAccordingly(Logger log, DirectedGraph<Integer,DefaultEdge> inputGraph, int nVertices){
+
+        // First step: order the leaves in accordance of their left-right appearance in the Tree
+        List<PartitiveFamilyLeafNode> orderedLeaves = new ArrayList<>(nVertices);
+        this.getLeavesInLeftToRightOrder(orderedLeaves);
+        ArrayList<Integer> permutationAsIntegers = new ArrayList<>(nVertices);
+        for(PartitiveFamilyLeafNode leaf : orderedLeaves){
+            permutationAsIntegers.add(leaf.getVertex());
+        }
+        // the position of every element in the permutation
+        int[] positionInPermutation = new int[nVertices];
+        for(int i = 0; i< permutationAsIntegers.size(); i++){
+            positionInPermutation[permutationAsIntegers.get(i)] = i;
+        }
+
         PartitiveFamilyTreeNode rootNode = (PartitiveFamilyTreeNode) root;
-        rootNode.reorderAllInnerNodes( log, outNeighbors, inNeighbors,  orderedLeaves, positionInPermutation);
+
+
+        // now, iterate through the tree and compute for every inner node X of T_H:
+        //   - le(X), re(X): the first occurence of any vertex of X in σ.
+        //     this can be done bottom-up
+
+        // init
+        for(int i = 0; i<orderedLeaves.size(); i++){
+            PartitiveFamilyLeafNode leaf = orderedLeaves.get(i);
+            leaf.le_X = i;
+            leaf.re_X = i;
+            // can also be done here
+            leaf.lc_X = i;
+            leaf.rc_X = i;
+        }
+        // compute
+        rootNode.computeReAndLe();
+
+
+        //   - lc(X), rc(X): the leftmost/rightmost of its cutters
+        // Therefore: "BucketSort" edges of G according to σ. BitSets guarantee easy symdiff operation.:
+
+        // This is for N_{+}: 1st key is outVertex, 2nd key is destVertex
+        BitSet[] sortedOutEgdes = SortAndCompare.edgesSortedByPerm(permutationAsIntegers, positionInPermutation, inputGraph, true);
+        // This is for N_{-}: 1st key is destVertex, 2nd outVertex
+        BitSet[] sortedInEdges = SortAndCompare.edgesSortedByPerm(permutationAsIntegers, positionInPermutation,  inputGraph, false);
+
+
+
+        // now, compute the cutters:
+        rootNode.computeLeftRightCutterForThis(sortedOutEgdes, sortedInEdges);
+
+        // remember: X is a module iff le(X) == lc(X) and re(X) == rc(X)
+        // next step: use N_{+} and N_{-} to separate the children of a 0/1-complete node that is a module into R_X-classes
+        // todo: according to step 5, I can select any representative of its children. Is my subgraph okay???
+        rootNode.reorderAllInnerNodes( log, sortedOutEgdes, sortedInEdges,  orderedLeaves, positionInPermutation);
+
+    }
+
+    /**
+     * Implementation of the Inclusion tree according to Lemma 11
+     *
+     * @param inputSet a Collection of BitSets representing a partitive set family
+     * @return The rooted tree of the input set
+     */
+    private void createInclusionTreeFromBitsets(Collection<BitSet> inputSet, Logger log, int nVertices) {
+
+        HashMap<BitSet, RootedTreeNode> bitsetToInclusionTreenNode = new HashMap<>(inputSet.size()*4/3);
+
+        // Step 0: eliminate doubles from input and add root, if not yet present.
+        HashSet<BitSet> inputSetNoDoubles = new HashSet<>(inputSet);
+        if (inputSetNoDoubles.size() != inputSet.size()) {
+            log.info("Double entry in input for inclusion tree - possible merge in overlap components.");
+        }
+        ArrayList<BitSet> inputNodeList = new ArrayList<>(inputSetNoDoubles);
+
+        // Sort the array by size, using counting sort
+        // todo: Das brauche ich häufiger! Auch hier mit BucketSort...
+        inputNodeList.sort( // descending size, with lamdas.
+                (b1, b2) -> Integer.compare(b2.cardinality(), b1.cardinality()));
+        log.fine("Input sorted by size: " + inputNodeList);
+
+        // add root, if not yet included
+        BitSet rootSet = new BitSet(nVertices);
+        rootSet.set(0, nVertices); // toIndex must be n
+        if(!inputSetNoDoubles.contains(rootSet)) {
+            inputNodeList.add(0, rootSet);
+        }
+
+
+        // Create a List for each v ∈ V of the members of F (i.e. the elements of σ) containing v in ascending order of their size:
+
+        // init empty
+        ArrayList<LinkedList<BitSet>> xLists = new ArrayList<>(nVertices);
+        for (int i = 0; i < nVertices; i++) {
+            xLists.add(i, new LinkedList<>());
+        }
+
+
+
+        // - visit each Y ∈ F in descending order of size. For each x ∈ Y, insert pointer to Y to front of x's list. [O(sz(F)]
+        inputNodeList.forEach(ySet ->
+                ySet.stream().forEach(i ->
+                        xLists.get(i).addFirst(ySet)
+                )
+        );
+        // root is now the last element in every xList
+
+
+        // PartitiveFamilyTree ret = new PartitiveFamilyTree(); this
+        PartitiveFamilyTreeNode root = new PartitiveFamilyTreeNode(rootSet);
+        this.setRoot(root);
+        //HashMap<BitSet, RootedTreeNode> bitsetToOverlapTreenNode = new HashMap<>(nontrivOverlapComponents.size() * 4 / 3);
+        bitsetToInclusionTreenNode.put(rootSet, root);
+
+        // This creates the inclusion tree from the x's lists
+        // - visit each x ∈ V, put a parent pointer from each member of x's list to its successor in x's list (if not already done)
+        //      -> these are chains of ancestors of {x}
+
+        PartitiveFamilyLeafNode[] allLeafs = new PartitiveFamilyLeafNode[nVertices]; // todo: was damit? Muss ja mal bottom-up?
+        int relationCount = 0;
+        for (int vertexNr = 0; vertexNr < nVertices; vertexNr++) {
+
+            // retrieve List and create leafNode
+            LinkedList<BitSet> currVertexList = xLists.get(vertexNr);
+            PartitiveFamilyLeafNode leafNode = new PartitiveFamilyLeafNode(vertexNr);
+
+            boolean firstEntry = true;
+            ListIterator<BitSet> vListIter = currVertexList.listIterator(0);
+            while (vListIter.hasNext()) {
+
+                BitSet currModule = vListIter.next();
+                // create Trenode if not yet present
+                PartitiveFamilyTreeNode currTreenode = (PartitiveFamilyTreeNode) bitsetToInclusionTreenNode.getOrDefault(currModule, null);
+                if (currTreenode == null) {
+                    currTreenode =  new PartitiveFamilyTreeNode(currModule);
+                    bitsetToInclusionTreenNode.put(currModule, currTreenode);
+                }
+
+                // the first entry of the list always gets the leaf attached.
+                if (firstEntry) {
+                    currTreenode.addChild(leafNode);
+                    allLeafs[vertexNr] = leafNode;
+                    firstEntry = false;
+                }
+
+                // add as child to the next element of xList
+                // Note: currTreenode.equals(root) occurs when we're at the end of the list
+                // Don't check for parent of root, of course.
+                if (vListIter.hasNext()) {
+                    BitSet parentModule = vListIter.next();
+                    PartitiveFamilyTreeNode parentTreeNode = (PartitiveFamilyTreeNode) bitsetToInclusionTreenNode.get(parentModule);
+
+                    // create parent if not yet present
+                    if (parentTreeNode == null) {
+                        parentTreeNode = new PartitiveFamilyTreeNode(parentModule);
+                        bitsetToInclusionTreenNode.put(parentModule, parentTreeNode);
+                        log.fine("Vertex " + vertexNr + ": Created Parent " + parentModule);
+                    }
+
+                    // add parent if current treenode has no parent
+                    if (currTreenode.isRoot()) {
+                        parentTreeNode.addChild(currTreenode);
+                        relationCount++;
+                        log.fine("Vertex " + vertexNr + ": Rel " + relationCount + ": child " + currModule + " to parent " + parentModule);
+                    }
+                    // go one element back in list
+                    vListIter.previous();
+                }
+
+            }
+
+        }
+        setModuleToTreenode(bitsetToInclusionTreenNode);
     }
 
 
